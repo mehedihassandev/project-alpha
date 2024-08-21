c68c26b4a246ddf5c5de0f7a73fb88d78a8520c7
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-08-21 16:07:13 +0600

    P-09: Add font and configure the theme and update json data file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,17 @@ export default {
     theme: {
         extend: {
             colors: {
-                primary: "#12171D",
+                // primary: "#12171D",
+                primary: "#11161E",
                 secondary: "#19E684",
-                textColor: "#94A3B8",
+                // textColor: "#94A3B8",
+                textColor: "#fff",
                 buttonColor: "#19E684",
             },
             fontFamily: {
                 saira: ['"Saira"', "sans-serif"],
                 syne: ['"Syne"', "sans-serif"],
+                poppins: ['"Poppins"', "sans-serif"],
             },
         },
     },
