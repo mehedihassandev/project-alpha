6ddf1ec99286535a24ee79bb30b1d9a91a03ff23
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-10-03 13:31:05 +0600

    P-18: Refactor main.jsx to include a NotificationProvider for displaying notifications

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ErrorBoundary } from "./app/components/ErrorBoundary.jsx";
+import { NotificationProvider } from "@utils/hooks/notification-hook.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <NotificationProvider>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </NotificationProvider>
   </React.StrictMode>,
 );
