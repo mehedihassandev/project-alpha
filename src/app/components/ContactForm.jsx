6c0bc5e8b3c96ff6fbb23ddd761f3030995ce131
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-10-03 13:32:14 +0600

    P-18: Refactor package.json to add formik and yup dependencies

--- src/app/components/ContactForm.jsx
+++ src/app/components/ContactForm.jsx
@@ -1,56 +1,65 @@
 import { useState } from "react";
 import emailjs from "emailjs-com";
+import { useNotification } from "@utils/hooks/notification-hook";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { Loader } from "./Loader";
+
+const validationSchema = yup.object({
+  name: yup.string().required("Full Name is required"),
+  email: yup.string().email("Invalid email address").required("Email is required"),
+  message: yup.string().required("Message is required"),
+});
 
 export const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
+  const notify = useNotification();
+  const [loading, setLoading] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      message: ''
+    },
+    validationSchema,
+    onSubmit: (values, { resetForm }) => {
+      setLoading(true);
+
+      const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+      const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+      const USER_ID = import.meta.env.VITE_EMAILJS_USER_ID;
+
+      const templateParams = {
+        name: values.name,
+        email: values.email,
+        message: values.message,
+      };
+
+      emailjs.send(
+        SERVICE_ID,
+        TEMPLATE_ID,
+        templateParams,
+        USER_ID
+      ).then(
+        (result) => {
+          console.log(result.text);
+          notify('Message sent successfully!', true);
+          resetForm();
+        },
+        (error) => {
+          console.log(error.text);
+          notify('Failed to send message.', false);
+        }
+      ).finally(() => {
+        setLoading(false);
+      });
+    },
   });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-    const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-    const USER_ID = import.meta.env.VITE_EMAILJS_USER_ID;
-
-    const templateParams = {
-      name: formData.name,
-      email: formData.email,
-      message: formData.message,
-    };
-
-    emailjs.send(
-      SERVICE_ID, // Replace with your EmailJS service ID
-      TEMPLATE_ID, // Replace with your EmailJS template ID
-      templateParams, // Pass the templateParams object
-      USER_ID // Replace with your EmailJS user ID
-    ).then(
-      (result) => {
-        console.log(result.text);
-        alert("Message sent successfully!");
-      },
-      (error) => {
-        console.log(error.text);
-        alert("Failed to send message.");
-      }
-    );
-  };
-
 
   return (
     <div className="box rounded-xl">
       <div className="w-full px-8 py-5 pb-10 mx-auto overflow-hidden bg-primary rounded-xl">
-        <form className="mt-4 w-full" onSubmit={sendEmail}>
+        <form className="mt-4 w-full" onSubmit={formik.handleSubmit}>
           <div className="flex-1">
             <label className="block mb-2 text-md text-textColor font-saira">
               Full Name
@@ -58,11 +67,15 @@ export const ContactForm = () => {
             <input
               type="text"
               name="name"
-              value={formData.name}
-              onChange={handleChange}
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               placeholder="John Doe"
               className="block w-full px-5 py-3 mt-2 text-white border rounded-md bg-gray-900 border-secondary focus:border-secondary focus:ring-secondary focus:ring-opacity-40 focus:outline-none focus:ring font-syne"
             />
+            {formik.touched.name && formik.errors.name ? (
+              <div className="text-red-500">{formik.errors.name}</div>
+            ) : null}
           </div>
           <div className="flex-1 mt-6">
             <label className="block mb-2 text-md text-textColor font-saira">
@@ -71,11 +84,15 @@ export const ContactForm = () => {
             <input
               type="email"
               name="email"
-              value={formData.email}
-              onChange={handleChange}
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               placeholder="johndoe@example.com"
               className="block w-full px-5 py-3 mt-2 text-white border rounded-md bg-gray-900 border-secondary focus:border-secondary focus:ring-secondary focus:ring-opacity-40 focus:outline-none focus:ring font-syne"
             />
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-red-500">{formik.errors.email}</div>
+            ) : null}
           </div>
           <div className="w-full mt-6">
             <label className="block mb-2 text-md text-textColor font-saira">
@@ -85,14 +102,24 @@ export const ContactForm = () => {
               className="block w-full h-32 px-5 py-3 mt-2 text-white placeholder-gray-400 border border-secondary rounded-md md:h-48 bg-gray-900 focus:border-secondary focus:ring-secondary focus:ring-opacity-40 focus:outline-none focus:ring font-syne"
               placeholder="Message"
               name="message"
-              value={formData.message}
-              onChange={handleChange}
+              value={formik.values.message}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             ></textarea>
+            {formik.touched.message && formik.errors.message ? (
+              <div className="text-red-500">{formik.errors.message}</div>
+            ) : null}
           </div>
-          <button className="w-full px-6 py-3 mt-6 text-md font-medium tracking-wide text-black capitalize transition-colors duration-300 transform bg-secondary rounded-md hover:bg-primary hover:text-white focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50 font-saira">
-            get in touch
+          <button
+            type="submit"
+            className="w-full px-6 py-3 mt-6 text-md font-medium tracking-wide text-black capitalize transition-colors duration-300 transform bg-secondary rounded-md hover:bg-primary hover:text-white focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50 font-saira"
+            disabled={!formik.isValid || loading}
+          >
+            {loading ? "Sending..." : "Get in touch"}
           </button>
         </form>
+
+        {loading && (<Loader />)}
       </div>
     </div>
   );
